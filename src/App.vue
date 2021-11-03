150c8d3f9b61ad0e766ec38702296005059c09db
YUU-KUN <iwahyu@student.telkomuniversity.ac.id>
2021-11-04 00:59:03 +0700

    ready for deployment

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/gizzard.png"  width="30%" >
+    <HelloWorld msg="Yuuu.dev"/>
   </div>
 </template>
 
@@ -17,12 +17,17 @@ export default {
 </script>
 
 <style>
+@media (max-width: 768px) {
+  img {
+    width: 75%;
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 12%;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,14 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <div style="font-size: 60px; margin-top: 2%;">{{msg}}</div>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Y-yeah. . .<br>
+      This is it. My Portfolio Website.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    <ul style="margin: 3% 0">
+      <li><a href="https://github.com/YUU-KUN" target="_blank" rel="noopener">Github</a></li>
+      <li><a href="https://www.linkedin.com/in/indra-wahyu-577185194" target="_blank" rel="noopener">LinkedIn</a></li>
+      <li><a href="mailto:iwahyu809@gmail.com" target="_blank" rel="noopener">Email</a></li>
     </ul>
   </div>
 </template>
@@ -39,6 +24,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+ul li {
+  margin: 0 2em;
+}
 h3 {
   margin: 40px 0 0;
 }
